970be127643bd4b7da6c5253dbbc1ae5750bb246
YeeeeahRight <yeeeeeah.right@gmail.com>
2021-06-09 02:38:37 +0300

    add jwt token provider (jwt logic)

---
+++ service/src/main/java/com/epam/esm/service/logic/security/JwtTokenProvider.java
@@ -0,0 +1,77 @@
+package com.epam.esm.service.logic.security;
+
+import com.epam.esm.persistence.model.entity.Role;
+import com.epam.esm.service.exception.InvalidJwtException;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Set;
+
+@Component
+public class JwtTokenProvider {
+    private final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+    @Value("${jwt.duration}")
+    private long durationTime;
+
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public JwtTokenProvider(@Qualifier("securityUserService") UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String username, Set<Role> roles) {
+        Claims claims = Jwts.claims();
+        claims.setSubject(username);
+        claims.put("roles", roles.toString());
+        Date currentTime = new Date();
+        Date expirationTime = new Date(currentTime.getTime() + durationTime);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(currentTime)
+                .setExpiration(expirationTime)
+                .signWith(SIGNATURE_ALGORITHM, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            Date expirationDate = claimsJws.getBody().getExpiration();
+            Date currentDate = new Date();
+
+            return !expirationDate.before(currentDate);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new InvalidJwtException("JWT token is expired or invalid");
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        String username = getUsername(token);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+}
